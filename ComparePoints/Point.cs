e6b58ce2ffc83aaab25458b4a2030548fce28bf9
Vadim Shvanov <adm.shwan@gmail.com>
2020-10-11 11:18:36 +0200

    Implemented IComparable

---
+++ ComparePoints/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ComparePoints
+{
+	public class Point : IComparable<Point>
+	{
+		public int X { get; }
+		public int Y { get; }
+
+		public Point(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public int CompareTo(Point other)
+		{
+			return Math.Sign(Math.Sqrt(X * X + Y * Y) - Math.Sqrt(other.X * other.X + other.Y * other.Y));
+		}
+
+		public override string ToString()
+		{
+			return $"[{X}; {Y}]";
+		}
+	}
+}
---
+++ ComparePoints/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparePoints
+{
+	class Program
+	{
+		static void Main()
+		{
+			var points = new List<Point>
+			{
+				new Point(3, 3),
+				new Point(1, 2)
+			};
+
+			if(points[0].CompareTo(points[1]) > 0)
+			{
+				var tempPoint = points[0];
+				points[0] = points[1];
+				points[1] = tempPoint;
+			}
+
+			Console.WriteLine("Упорядоченные по возрастанию от начала координат точки:");
+			foreach(var point in points)
+			{
+				Console.WriteLine(point);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
