b5dd400b2503ae109fe709ac1896a2bc6545f62a
Vadim Shvanov <adm.shwan@gmail.com>
2020-11-15 11:35:29 +0100

    Явная реализация интерфейсного метода

---
+++ ExtinctInterfaceMethodImplementation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExplicitInterfaceMethodImplementation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var simpleType = new SimpleType();
+
+			// Вызов неявной реализации.
+			simpleType.Dispose();
+
+			// Вызов явной реализации.
+			IDisposable disposable = simpleType;
+			disposable.Dispose();
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ExtinctInterfaceMethodImplementation/SimpleType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExplicitInterfaceMethodImplementation
+{
+	public class SimpleType : IDisposable
+	{
+		// Неявная реализация метода.
+		public void Dispose()
+		{
+			Console.WriteLine("SimpleType Dispose()");
+		}
+
+		// Явная реализация метода.
+		void IDisposable.Dispose()
+		{
+			Console.WriteLine("IDisposable Dispose()");
+		}
+	}
+}
